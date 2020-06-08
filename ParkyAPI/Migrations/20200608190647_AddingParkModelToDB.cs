21f0485f9bfbcb68a13c80f2fbda9a260f3c619b
Vinod Patil <A00268849@student.ait.ie>
2020-06-08 20:10:19 +0100

    Database Connection and NPark Model Class Migration

---
+++ ParkyAPI/Migrations/20200608190647_AddingParkModelToDB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkyAPI.Migrations
+{
+    public partial class AddingParkModelToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NParks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    State = table.Column<string>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Established = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NParks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NParks");
+        }
+    }
+}
