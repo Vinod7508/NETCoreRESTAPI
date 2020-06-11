58bec8d0a7acaecda378cbe2c80bd0e902f2778f
Vinod Patil <A00268849@student.ait.ie>
2020-06-11 20:36:18 +0100

    Controller Update

--- ParkyAPI/Controllers/NParkController.cs
+++ ParkyAPI/Controllers/NParkController.cs
@@ -12,8 +12,8 @@ namespace ParkyAPI.Controllers
 {
     [Route("api/[controller]")]  //access using API/Controller name
 
- // API forward slash the controller name the controller name is national parks.
- //So if you type API forward slash national parks that will be the default road for all of the action methods here and you can see it inherits from controller base.You can change it to inherit just from the controller.
+    // API forward slash the controller name the controller name is national parks.
+    //So if you type API forward slash national parks that will be the default road for all of the action methods here and you can see it inherits from controller base.You can change it to inherit just from the controller.
 
 
     [ApiController]
@@ -31,23 +31,45 @@ namespace ParkyAPI.Controllers
 
 
         [HttpGet]
-        public IActionResult GetNationalParks()    
+        public IActionResult GetNationalParks()
         {
-            var nparkdata = _nParkRepository.GetAllNParks();  
+            var nparkdata = _nParkRepository.GetAllNParks();
             //we should never exposed our domain model to outiside word...
-           // we should only exposed DTOS...another approach..using mapper
+            // we should only exposed DTOS(another approach)using mapper.
 
             var objdto = new List<NParkDto>();
 
-            foreach(var obj in nparkdata)
+            foreach (var obj in nparkdata)
             {
                 objdto.Add(_mapper.Map<NParkDto>(obj));  //here we are mapping our domain class
+
+
+            }
+            //return Ok(nparkdata);    //here we are returining list from our domain class models.nparks.
+
+            return Ok(objdto);   //here we are returining list from our dto object.
+        }
+
+
+
+        //get single National Park
+        [HttpGet("{NParkId:int}")]// routingS
+        public IActionResult GetSingleNationalParks(int NParkId)
+        {
+
+            var singlepark = _nParkRepository.GetPark(NParkId);
+            if (singlepark == null)
+            {
+                return NotFound();
             }
 
+            var singleparkDto = _mapper.Map<NParkDto>(singlepark);
+            return Ok(singleparkDto);
 
-            //return Ok(nparkdata);    //here we are returining list from our domain class models.nparks.
-            return Ok(objdto);  //here we are returining list from our dto object.
         }
+         
+
+
 
     }
 }
