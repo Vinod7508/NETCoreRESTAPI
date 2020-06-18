383e1835579a55cfc0008b62e7630295274823b3
Vinod Patil <A00268849@student.ait.ie>
2020-06-18 01:12:04 +0100

    Trail Model and Dto addition

---
+++ ParkyAPI/Models/Trail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Models
+{
+    public class Trail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Distance { get; set; }
+
+
+        public enum DifficultyType {
+            Easy,
+            Moderate,
+            Difficult,
+            Expert
+        }
+
+        public DifficultyType Difficulty { get; set; }
+
+        [Required]
+        public int NParkId  { get; set; }
+
+        [ForeignKey("NParkId")]
+        public NPark NPark { get; set; }
+
+        public DateTime DateCreated { get; set; }  //we are not exposing this property to outsideword/ftos
+
+
+    }
+}
